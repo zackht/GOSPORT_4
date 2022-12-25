3969b7b57f18b1877973e40578eba5f69bfa9eaf
jeff578210 <jeff50000123@gmail.com>
2022-12-25 20:26:21 +0800

    新增後台球隊頁面 李

---
+++ gosport/src/back/backarticleteam.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import backteam from './backarticleteam.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import group41 from "./icon/Group 41.png";
+class Backteam extends Component {
+    state = {}
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default Backteam;
