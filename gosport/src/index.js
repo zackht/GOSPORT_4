9bf7ff6072f16cac89d7fcd3993f403b20488990
jeff578210 <jeff50000123@gmail.com>
2022-12-25 16:59:36 +0800

    更新index.js 李

--- gosport/src/index.js
+++ gosport/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Side2 from './side/rentsid_more';
+import Backqa from './back/backqa';
+import Testt from './side/testt';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Side2/> */}
+    {/* <Testt /> */}
+    <Backqa/>
   </React.StrictMode>
 );
 
