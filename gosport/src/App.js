3d32a1f6a1cd908727c857c8fc15c678cc812e35
Lin <wwvvwlliil@gmail.com>
2022-12-28 10:40:54 +0800

    20221228 1040 teams/basic

--- gosport/src/App.js
+++ gosport/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+<<<<<<< HEAD
  
 
 import backqa from './back/backqa';
 import Backarticle from './back/backarticle';
+=======
+import Basic from './teams/basic.jsx';
+>>>>>>> 5805181 (2022.12.27 16:39 Basic完成)
 // import Navigation from './components/Navigation';
  
 class App extends Component {
@@ -13,7 +17,13 @@ class App extends Component {
         <div>
           {/* <Navigation /> */}
             <Switch>
+<<<<<<< HEAD
              <Route path="/Backarticle" component={Backarticle}/>
+=======
+             
+            <Route path="/teams/basic" component={Basic}/>
+            
+>>>>>>> 5805181 (2022.12.27 16:39 Basic完成)
            </Switch>
         </div> 
       </BrowserRouter>
