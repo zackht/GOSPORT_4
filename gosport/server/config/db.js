7b3b4c5c5fe3cc93b29f4a5cd613b95f166440e6
jeff578210 <120230137+jeff578210@users.noreply.github.com>
2022-12-30 13:33:29 +0800

    Delete gosport/server directory

--- gosport/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-// connect MySQL
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "gosport",
-});
-module.exports = connection;
