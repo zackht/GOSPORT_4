a8235df832a82e77dd3da539a2a1ff5cdc8c1760
Lin <wwvvwlliil@gmail.com>
2023-01-18 10:25:45 +0800

    0118 zhi header&footer css

--- gosport/src/headerfooter/footer.js
+++ gosport/src/headerfooter/footer.js
@@ -6,7 +6,7 @@ export default function Footer(props){
     return(
         <div className="footer">
             <div>
-                <div>Copyright © 2022 GOsport. 保留一切權利。</div>
+                <div>Copyright © 2023 GOsport. 保留一切權利。</div>
             </div>
         </div>
     )
