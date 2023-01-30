ab5158f48ebcac72943017b689bd82ad906c7df5
jeff578210 <jeff50000123@gmail.com>
2023-01-30 13:23:10 +0800

    123321

--- gosport/src/side/rentside.jsx
+++ gosport/src/side/rentside.jsx
@@ -10,8 +10,16 @@ import a4 from "./icon/方向 (2).png";
 import a5 from "./icon/Group 41.png";
 import a6 from './icon/Group.png';
 import Axios from "axios";
-
+import bask from './icon/bask.svg';
+import tennis from './icon/Group 2.svg';
+import { useEffect } from 'react';
 const Rentside = () => {
+    // useLayoutEffect
+    // useEffect(() => {
+    //     console.log('快速搜尋');
+    //     if(){
+    //     }
+    //   }, [])
     const Taichung = [
         "不限","中區", "東區", "西區", "南區", "北區", "西屯區", "南屯區", "北屯區", "豐原區", "大里區", "太平區", "清水區", "沙鹿區", "大甲區", "東勢區", "梧棲區", "烏日區", "神岡區", "大肚區", "大雅區", "后里區", "霧峰區", "潭子區", "龍井區", "外埔區", "和平區", "石岡區", "大安區", "新社區"
     ];
@@ -100,7 +108,7 @@ const Rentside = () => {
                             <div id="local" className={cc.d2}>
                                 <p className={cc.d3}>類別</p>
                                 <div className={cc.d4d}>
-                                    <img className={cc.d4} src={a6} alt="" /><br />
+                                    <img className={cc.d4} src={`${type=='羽球'?a6:type=='桌球'?tennis:bask}`} alt="" /><br />
                                     <select name="" id="aaa" className={cc.d4s} onChange={(e)=>{settype(e.target.value)}}>
                                         <option value="羽球">羽球</option>
                                         <option value="桌球">桌球</option>
