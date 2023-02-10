4c75c3542d4738604679a1c738ad14567db60434
JoeWen135 <joewen13579246@gmail.com>
2023-02-10 22:49:29 +0800

    個人資料scrolling 黃

--- gosport/src/selfpage/selfpage.js
+++ gosport/src/selfpage/selfpage.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './selfpage.css'
 import Axios from "axios";
 import Cookies from 'js-cookie';
@@ -69,53 +69,82 @@ const Selfpage = () => {
         }
     }, [selfInfo])
 
+    // 畫面位移
+    const [positionX, setPositionX] = useState({ x: 0});
+    const [positionY, setPositionY] = useState({ y: 0});
+    const screenWidth = window.innerWidth/2;
+    const screenHight = window.innerHeight/2; 
+
+    useEffect(() => {
+      const handleMouseMove = (event) => {
+        if(event.clientX > screenWidth) //left
+        setPositionX({ x: -(event.clientX-screenWidth)/50 });
+        if(event.clientY > screenHight) //top
+        setPositionY({ y: (event.clientY-screenHight)/50 });
+        if(event.clientX < screenWidth) //right
+        setPositionX({ x: (screenWidth-event.clientX)/50 });
+        if(event.clientY < screenHight) //bottom
+        setPositionY({ y: -(screenHight-event.clientY)/50 });
+      };
+      document.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+      };
+    }, []);
+
     return (
         <React.Fragment>
-                {/* 主體 */}
-                <div className='self_'>
-                    <div className="self_cover">
-                        <div style={{ display: 'flex', width: '90%', height: '481px' }}>
-                            <div className="self_discribe">
-                                <div>姓名</div>
-                                <div>{selfInfo[0].username !== null ? selfInfo[0].username : '尚無'}</div>
-                                <div>程度</div>
-                                <div>
-                                    羽球{selfInfo[0].badminton !== null ? selfInfo[0].badminton : '未知'}、
-                                    桌球{selfInfo[0].tabletennis !== null ? selfInfo[0].tabletennis : '未知'}、
-                                    排球{selfInfo[0].volleyball !== null ? selfInfo[0].volleyball : '未知'}
-                                </div>
-                                <div>球隊</div>
-                                <div>
-                                    {alltname.length !== 0 ? showtname : '尚無'}
-                                </div>
-                                <div>活動時數</div>
-                                <div>{selfInfo[0].activeTime !== null ? selfInfo[0].activeTime : '尚無'}</div>
-                                <div>描述</div>
-                                <article className='describe'>
-                                    {selfInfo[0].userdescribe}
-                                </article>
+            {/* 主體 */}
+            <div className='self_'>
+                <div className="self_cover" >
+                    <div className="self_move">
+                        <div className="self_discribe"
+                        style={{
+                            position : 'relative',
+                            top: `${positionY.y}px`,
+                            right: `${positionX.x}px`,
+                        }}>
+                            <div>姓名</div>
+                            <div>{selfInfo[0].username !== null ? selfInfo[0].username : '尚無'}</div>
+                            <div>程度</div>
+                            <div>
+                                羽球{selfInfo[0].badminton !== null ? selfInfo[0].badminton : '未知'}、
+                                桌球{selfInfo[0].tabletennis !== null ? selfInfo[0].tabletennis : '未知'}、
+                                排球{selfInfo[0].volleyball !== null ? selfInfo[0].volleyball : '未知'}
                             </div>
-                            <div className="self_right" >
-                                <div className='self_imgbox'>
-                                    <span>
-                                        <img className='self_img' src={userurl} alt="" />
-                                    </span>
-                                    <div className="show_star" style={{ display: selfBadge[0].badgeurl !== '' ? 'flex' : 'none' }}>
-                                        {selfBadge.map(item => <img key={item.badgeid} src={item.badgeurl} alt=''></img>)}
-                                        {/* <embed src={star}></embed> */}
-                                        {/* <embed src={star}></embed> */}
-                                        {/* <embed src={star}></embed> */}
-                                    </div>
+                            <div>球隊</div>
+                            <div>
+                                {alltname.length !== 0 ? showtname : '尚無'}
+                            </div>
+                            <div>活動時數</div>
+                            <div>{selfInfo[0].activeTime !== null ? selfInfo[0].activeTime : '尚無'}</div>
+                            <div>描述</div>
+                            <article className='describe'>
+                                {selfInfo[0].userdescribe}
+                            </article>
+                        </div>
+                        <div className="self_right" >
+                            <div className='self_imgbox'>
+                                <span>
+                                    <img className='self_img' src={userurl} alt="" />
+                                </span>
+                                <div className="show_star" style={{ display: selfBadge[0].badgeurl !== '' ? 'flex' : 'none' }}>
+                                    {selfBadge.map(item => <img key={item.badgeid} src={item.badgeurl} className='self_badge' alt=''></img>)}
+                                    {/* <embed src={star}></embed> */}
+                                    {/* <embed src={star}></embed> */}
+                                    {/* <embed src={star}></embed> */}
                                 </div>
-                                <div style={{ height: "50%", width: "100%", position: "relative" }}>
-                                    <Link to="/gosport/user/settings">
-                                        <button className="self_edit" style={{ position: "absolute", bottom: "0px", right: "0px" }}>編輯</button>
-                                    </Link>
-                                </div>
+                            </div>
+                            <div style={{ height: "50%", width: "100%", position: "relative" }}>
+                                <Link to="/gosport/user/settings">
+                                    <button className="self_edit" style={{ position: "absolute", bottom: "0px", right: "0px" }}>編輯</button>
+                                </Link>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </React.Fragment>
     );
 }
--- gosport/src/selfpage/selfroute.jsx
+++ gosport/src/selfpage/selfroute.jsx
@@ -28,7 +28,7 @@ const Selfroute = () => {
     }]);
     const [selfteam, setSelfTeam] = useState([{ tname: '' }]);
     const [selfBadge, setSelfBadge] = useState([{ badgeid: '', badgeurl: '' }]);
-     useEffect(() => {
+    useEffect(() => {
         console.log(id)
         Axios.post("http://localhost:3001/self", {
             userid: userid,
@@ -66,6 +66,36 @@ const Selfroute = () => {
         }
     }, [selfInfo])
 
+    // 畫面位移
+    const [positionX, setPositionX] = useState({ x: 0 });
+    const [positionY, setPositionY] = useState({ y: 0 });
+    const screenWidth = window.innerWidth / 2;
+    const screenHight = window.innerHeight / 2;
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            if (event.clientX > screenWidth) //left
+                setPositionX({ x: -(event.clientX - screenWidth) / 50 });
+            if (event.clientY > screenHight) //top
+                setPositionY({ y: (event.clientY - screenHight) / 50 });
+            if (event.clientX < screenWidth) //right
+                setPositionX({ x: (screenWidth - event.clientX) / 50 });
+            if (event.clientY < screenHight) //bottom
+                setPositionY({ y: -(screenHight - event.clientY) / 50 });
+        };
+        document.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    let date = new Date(Date.UTC(2022, 1, 1, 12, 30, 15));
+    // let date = Date.now();
+    let formattedDate = date.toLocaleDateString('zh-TW', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    formattedDate = formattedDate.split('/').join('/');
+    console.log(formattedDate); // Output: "2022/01/01" 
+    // console.log(date)
 
     return (
         <React.Fragment>
@@ -73,7 +103,11 @@ const Selfroute = () => {
             <div className='self_'>
                 <div className="self_cover">
                     <div style={{ display: 'flex', width: '90%', height: '481px' }}>
-                        <div className="self_discribe">
+                        <div className="self_discribe" style={{
+                            position: 'relative',
+                            top: `${positionY.y}px`,
+                            right: `${positionX.x}px`,
+                        }}>
                             <div>姓名</div>
                             <div>{selfInfo[0].username !== null ? selfInfo[0].username : '尚無'}</div>
                             <div>程度</div>
@@ -99,11 +133,11 @@ const Selfroute = () => {
                                     <img className='self_img' src={userurl} alt="" />
                                 </span>
                                 <div className="show_star" style={{ display: selfBadge[0].badgeurl !== '' ? 'flex' : 'none' }}>
-                                    {selfBadge.map(item => <img key={item.badgeid} src={item.badgeurl} alt=''></img>)}
+                                    {selfBadge.map(item => <img key={item.badgeid} src={item.badgeurl} className='self_badge' alt=''></img>)}
                                 </div>
                             </div>
                             <div style={{ height: "50%", width: "100%", position: "relative" }}>
-                                <button className="self_edit" onClick={()=>{history.goBack()}}
+                                <button className="self_edit" onClick={() => { history.goBack() }}
                                     style={{ position: "absolute", bottom: "0px", right: "0px" }}>返回</button>
                             </div>
                         </div>
